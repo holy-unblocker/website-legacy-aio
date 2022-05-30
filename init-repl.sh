79e58c5bc31e924e689c5e73b277ca0af98d34c3
David Reed <x@sys32.dev>
2022-05-30 00:35:07 -0400

    disable eslint, scripts

--- init-repl.sh
+++ init-repl.sh
@@ -17,15 +17,14 @@ cd website
 cd ..
 
 npm install --save-dev node@17
-npm config set prefix=$(pwd)/node_modules/node
+npm config set prefix=$(repo)/node_modules/node
 
 cd node_modules/node/bin
 	echo "{}" > ./package.json
 	npm i npm@8
 cd $repo
 
-export PATH=$(pwd)/node_modules/node/bin:$PATH
-export PATH=$(pwd)/node_modules/node/bin/node_modules/npm/bin:$PATH
+./node-env.sh
 
 npm install
 npm run install-submodules
---
+++ node-env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export PATH=$(pwd)/node_modules/node/bin:$(pwd)/node_modules/node/bin/node_modules/npm/bin:$PATH
