86a5e8189e6a69754d325658103d1dd28ca7c406
David Reed <x@sys32.dev>
2022-05-30 15:19:26 -0400

    instructions,

--- scripts/install.js
+++ scripts/install.js
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import { website, rammerhead, bare_server } from '../config/paths.js';
 import { spawnAsync } from '../config/util.js';
 
@@ -16,4 +17,12 @@ await spawnAsync('npm', ['install'], {
 	cwd: bare_server,
 });
 
-console.log('Installed dependencies for scripts');
+console.log();
+console.log(chalk.green('Modules installed successfully.'));
+console.log();
+
+console.log(`You can now build ${chalk.bold('website-aio')}:`);
+console.log();
+
+console.log(`  ${chalk.cyan('npm')} run build`);
+console.log();
