a17a1c3d1bf70980eeb84d35afca7bdba02b6f72
David Reed <x@sys32.dev>
2022-05-29 23:36:19 -0400

    disable ssl

--- init-repl.sh
+++ init-repl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+git config --global http.sslverify false
 git submodule update --init --recursive
 npm install --save-dev node@18
 npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
