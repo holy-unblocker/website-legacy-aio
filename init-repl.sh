f4904b3f5a19f3c0580bd3b6a73f8edc2fe4be70
David Reed <x@sys32.dev>
2022-05-29 23:33:44 -0400

    ,

--- init-repl.sh
+++ init-repl.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+	
 git submodules update --init --recursive
 npm install --save-dev node@18
 npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
