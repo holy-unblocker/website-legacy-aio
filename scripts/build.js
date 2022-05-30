fa5881edb1b0f41f14dc5123cd10976a08e22d19
David Reed <x@sys32.dev>
2022-05-29 21:38:27 -0400

    build rammerhead

--- scripts/build.js
+++ scripts/build.js
@@ -1,10 +1,16 @@
 import '../config/prod.js';
 import '../config/env.js';
-import { website } from '../config/paths.js';
+import { rammerhead, website } from '../config/paths.js';
 import { spawnAsync } from '../config/util.js';
 
 await spawnAsync('npm', ['run', 'build'], {
 	stdio: 'inherit',
 	cwd: website,
+	env: process.env,
+});
+
+await spawnAsync('npm', ['run', 'build'], {
+	stdio: 'inherit',
+	cwd: rammerhead,
 	env: process.env,
 });
