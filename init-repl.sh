403f046edaa91c35a52ce50e080e01e72ef616d7
David Reed <x@sys32.dev>
2022-05-29 23:47:25 -0400

    latest

--- init-repl.sh
+++ init-repl.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+repo=$(pwd)
 git config --global http.sslverify false
 git submodule update --init --recursive
-npm install --save-dev node@18
-npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
+npm install --save-dev node@17
+npm config set prefix=$(pwd)/node_modules/node
+
+cd node_modules/node/bin
+	npm i npm@8
+cd $repo
+
+export PATH=$(pwd)/node_modules/node/bin:$PATH
+export PATH=$(pwd)/node_modules/node/bin/node_modules/npm/bin:$PATH
+
 npm install
