8bcbf5fb941fa084934fe8628a48cb0048cad71f
David Reed <x@sys32.dev>
2022-05-30 20:04:07 -0400

    check submodules

---
+++ no-git.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/tomphttp/bare-server-node bare-server-node --depth 1
+git clone https://git.holy.how/holy/website-fork-aio.git website --depth 1
+git clone https://github.com/e9x/rammerhead-fork-aio.git rammerhead --depth 1
--- scripts/install.js
+++ scripts/install.js
@@ -1,8 +1,28 @@
+import { open } from 'node:fs/promises';
+import { join } from 'node:path';
+
 import chalk from 'chalk';
 
 import appName from '../config/appName.js';
 import { bare_server, rammerhead, website } from '../config/paths.js';
 import spawnAsync from '../config/spawnAsync.js';
+
+// no package = install parent dir (run this script again) = infinite loop
+async function testSubmodule(dir, sm) {
+	try {
+		await open(join(dir, 'package.json'));
+	} catch (error) {
+		if (error.code === 'ENOENT') {
+			throw new Error(`Submodules were not updated and cloned. (${sm})`);
+		}
+
+		throw error;
+	}
+}
+
+await testSubmodule(website, 'website-aio-fork');
+await testSubmodule(rammerhead, 'rammerhead-fork-aio');
+await testSubmodule(bare_server, 'bare-server-node');
 
 await spawnAsync('npm', ['install'], {
 	stdio: 'inherit',
