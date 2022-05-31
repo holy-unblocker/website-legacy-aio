6a9a0fff8a77b0fe15e800becd96b5045618bc1a
David Reed <x@sys32.dev>
2022-05-30 20:05:37 -0400

    SSL

--- no-git.sh
+++ no-git.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+git config --global http.sslverify false
 git clone https://github.com/tomphttp/bare-server-node bare-server-node --depth 1
 git clone https://git.holy.how/holy/website-fork-aio.git website --depth 1
 git clone https://github.com/e9x/rammerhead-fork-aio.git rammerhead --depth 1
