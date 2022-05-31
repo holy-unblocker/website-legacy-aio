ebac831a43ecdf0cac0db2d149d1945be498a901
David Reed <x@sys32.dev>
2022-05-30 20:04:39 -0400

    perms

--- no-git.sh
+++ no-git.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 git clone https://github.com/tomphttp/bare-server-node bare-server-node --depth 1
 git clone https://git.holy.how/holy/website-fork-aio.git website --depth 1
 git clone https://github.com/e9x/rammerhead-fork-aio.git rammerhead --depth 1
